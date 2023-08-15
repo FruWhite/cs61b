1eec8996801541be8d7b0fd045d850fad47db4fd
fructus_white <fructuswhite@outlook.com>
2023-08-15 14:37:13 +0800

    fix output error

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -58,15 +58,15 @@ public class NBody {
             time += dt;
         }
 
+        for(Planet p : planets){
+            p.imgFileName =p.imgFileName.substring(7);
+        }
         StdOut.printf("%d\n", planets.length);
         StdOut.printf("%.2e\n", radius);
         for (int i = 0; i < planets.length; i++) {
             StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
                     planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
                     planets[i].yyVel, planets[i].mass, planets[i].imgFileName);}
-
-
-
 
     }
 }
